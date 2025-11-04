130bb19f3fd887a5461139036f964f53d7093817
ultimatedev03 <your_email@something.com>
2025-11-04 13:12:41 +0530

    commit 5004

--- src/main/java/com/itech/itech_backend/config/ProductionMailConfig.java
+++ src/main/java/com/itech/itech_backend/config/ProductionMailConfig.java
@@ -10,7 +10,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import java.util.Properties;
 
 @Configuration
-@Profile("production")
+@Profile({"production", "render"})
 public class ProductionMailConfig {
 
     @Value("${spring.mail.host:smtp.gmail.com}")
